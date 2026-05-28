0bb41a9f24ab1eb15b3cf19f0591a2e3c071629b
Antonio Guagliardi <antonio.guagliardi@rse-web.it>
2026-05-28 10:27:42 +0200

    rese piu' visibili le scritte su popup dati e variabili e fix variabili in settings

--- Alg_legopc/src/tix/openhelp.tcl
+++ Alg_legopc/src/tix/openhelp.tcl
@@ -4,28 +4,23 @@
 #
 #####################################################
 proc open_hlp { helpfile  } {
-	
+
 	global env
 
-	set browser {$env(LG_BROWSER)}
+	set browser $env(LG_BROWSER)
 	set hp $env(LG_HTML)
-	
+
 	set hf  [file join $hp $helpfile.htm]
 
-	if { ![file exists $env(LG_BROWSER)] } {
+	if { [auto_execok $browser] eq "" } {
 		tk_messageBox -type ok -message "HTML browser not found\nVerify your LG_BROWSER enviroment variable\nLG_BROWSER=$browser"
-		return 
+		return
 	}
 
 	if { ![file exists $hf]} {
 		tk_messageBox -type ok -message "Help file $hf  not found"
-		#set hf  [file join $hp index.htm]
-		#if { ![file exists $hf]} {
-		#	return 
-		#} 
-
 		return
-	} 
-	
-	eval exec $browser $hf &
+	}
+
+	exec $browser $hf &
 }
